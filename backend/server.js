cf5b83b78bfe3184c3d63be5adc1afbc064b021b
MoSam007 <samato.moma@gmail.com>
2024-04-30 13:08:18 +0300

    Updated files

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const upload = multer();
 const productRoute = require('./routes/api/productRoute');
 
 // Connecting to the Database
-let mongodb_url = 'mongodb://localhost/';
+let mongodb_url = 'mongodb+srv://samato:n70Lo8MZgJbCmnTu@cluster1.qjvlrbu.mongodb.net/?appName=mongosh+2.2.5';
 let dbName = 'yolomy';
 
 // define a url to connect to the database
